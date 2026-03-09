8e014311f0850723ad2e0f04dc1bbd90505452e9
원이 <hawon9781@gmail.com>
2026-03-09 09:55:17 +0900

    setting :: 프로젝트 생성

---
+++ NSG/NSG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NSG
+//
+//  Created by hawon on 3/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
