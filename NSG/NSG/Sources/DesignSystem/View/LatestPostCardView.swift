4040496e75bbebaca468250a2dbd476bed7ee518
원이 <hawon9781@gmail.com>
2026-03-27 09:14:53 +0900

    LatestPostCardView 컴포넌트 구현

--- NSG/NSG/Sources/DesignSystem/View/LatestPostCardView.swift
+++ NSG/NSG/Sources/DesignSystem/View/LatestPostCardView.swift
@@ -14,19 +14,19 @@ public final class LatestPostCardView: UIView {
         super.init(frame: .zero)
 
         backgroundColor = .black50
-        layer.cornerRadius = 12
+        layer.cornerRadius = 8
 
         let titleLabel = UILabel().then {
             $0.text = post.title
-            $0.font = .style(.header2)
+            $0.font = .style(.header3)
             $0.textColor = .black800
             $0.numberOfLines = 1
         }
 
         let contentLabel = UILabel().then {
             $0.text = post.content
-            $0.font = .style(.body2)
-            $0.textColor = .black500
+            $0.font = .style(.body3)
+            $0.textColor = .black800
             $0.numberOfLines = 1
         }
 
@@ -36,19 +36,19 @@ public final class LatestPostCardView: UIView {
         [titleLabel, contentLabel, chip, reactionView].forEach { addSubview($0) }
 
         titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(14)
-            $0.leading.trailing.equalToSuperview().inset(12)
+            $0.top.equalToSuperview().offset(15)
+            $0.leading.trailing.equalToSuperview().inset(11)
         }
 
         contentLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.leading.trailing.equalToSuperview().inset(12)
+            $0.leading.trailing.equalToSuperview().inset(11)
         }
 
         chip.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(11)
-            $0.top.equalTo(contentLabel.snp.bottom).offset(8)
-            $0.bottom.equalToSuperview().inset(11)
+            $0.top.equalTo(contentLabel.snp.bottom).offset(7)
+            $0.bottom.equalToSuperview().inset(10)
         }
 
         reactionView.snp.makeConstraints {
