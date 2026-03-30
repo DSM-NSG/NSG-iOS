937451ae878b0d7119ca79e8f320c286b6ae4f18
원이 <hawon9781@gmail.com>
2026-03-30 12:11:20 +0900

    마이페이지 수정

--- NSG/NSG/Sources/Scene/My/MyViewController.swift
+++ NSG/NSG/Sources/Scene/My/MyViewController.swift
@@ -50,16 +50,6 @@ final class MyViewController: UIViewController {
         configureUI()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
-
     private func addView() {
         [
             profileCardView,
