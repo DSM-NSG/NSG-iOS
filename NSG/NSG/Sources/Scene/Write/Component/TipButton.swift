2e0038703ef8622f2759734f38992a7e0005ed86
원이 <hawon9781@gmail.com>
2026-03-30 15:03:34 +0900

    꿀팁 작성 이미지 추가 및 title 추가

---
+++ NSG/NSG/Sources/Scene/Write/Component/TipButton.swift
@@ -0,0 +1,19 @@
+//
+//  TipButton.swift
+//  NSG
+//
+//  Created by hawon on 3/30/26.
+//
+import UIKit
+import SnapKit
+import Then
+
+final class TipButton: UIView {
+//    override init(frame: CGRect) {
+//        <#code#>
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+}
--- NSG/NSG/Sources/Scene/Write/TipSeletedViewController.swift
+++ NSG/NSG/Sources/Scene/Write/TipSeletedViewController.swift
@@ -5,10 +5,33 @@
 //  Created by hawon on 3/16/26.
 //
 import UIKit
+import SnapKit
+import Then
 
 class TipSeletedViewController: UIViewController {
+
+    private let titleLabel = UILabel().then {
+        $0.text = "꿀팁 작성"
+        $0.font = .style(.header1)
+        $0.textColor = .black800
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
+
+        addView()
+        setLayout()
+    }
+
+    private func addView() {
+        view.addSubview(titleLabel)
+    }
+
+    private func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(0)
+            $0.leading.equalToSuperview().inset(24)
+        }
     }
 }
